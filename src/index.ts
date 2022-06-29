163188a63bc9a7030e05cd63992848e7611c9c28
Ozan <77584235+ozancck@users.noreply.github.com>
2022-06-29 22:47:30 +0300

    CustomMap.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 //import { User } from './User'
 //import { Company } from './Company'
 
-new google.maps.Map(document.getElementById("map"),{
-    zoom:1,
-    center:{
-        lat: 0,
-        lng:0
-    }
-});
+import { CustomMap } from "./CustomMap";
+
+const customMap= new CustomMap()
+
+
+
 
 
 
