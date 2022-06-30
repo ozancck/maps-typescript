61a41108cc926f32fc8de1645ed7316b6509ae86
Ozan <77584235+ozancck@users.noreply.github.com>
2022-06-30 16:17:55 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@
 //import { Company } from './Company'
 
 import { CustomMap } from "./CustomMap";
+import { User } from "./User";
 
-const customMap= new CustomMap()
+const user= new User()
+const customMap= new CustomMap("map")
+
+customMap.addUserMarker(user)
 
 
 
