fed466511a2fe49959adc0774882d983525423cd
Ozan <77584235+ozancck@users.noreply.github.com>
2022-06-30 14:06:17 +0300

    Adding Markers

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,13 +1,29 @@
-export class CustomMap{
-   private googleMap: google.maps.Map;
+import { User } from './User';
+import { Company } from './Company';
 
-    constructor(divId: string){
-        this.googleMap= new google.maps.Map(document.getElementById(divId), {
-            zoom:1,
-            center:{
-                lat: 0,
-                lng:0
-            }
-        });
-    }
-}
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+
+  addCompanyMarker(company: Company): void {
+
+  }}
